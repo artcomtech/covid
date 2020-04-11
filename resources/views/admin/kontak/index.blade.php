40c5d00de884cb901932b4926826aa52f9e8a6ab
budiardianata <budiardianata@windowslive.com>
2020-04-11 12:41:58 +0700

    budi

--- resources/views/admin/kontak/index.blade.php
+++ resources/views/admin/kontak/index.blade.php
@@ -1,60 +1,61 @@
 @extends('admin.layout.master')
 @section('content')
-     <!-- Content Header (Page header) -->
-     <section class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
             <div class="col-sm-6">
-              <h1>Data Kontak</h1>
+                <h1>Data Kontak</h1>
             </div>
             <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#"></a></li>
-                <li class="breadcrumb-item active">Data Kontak</li>
-              </ol>
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#"></a></li>
+                    <li class="breadcrumb-item active">Data Kontak</li>
+                </ol>
             </div>
-          </div>
-        </div><!-- /.container-fluid -->
-      </section>
-      @if ($message = Session::get('success'))
-        <div class="alert alert-success alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button> 
-        <strong>{{ $message }}</strong>
         </div>
-      @endif
+    </div><!-- /.container-fluid -->
+</section>
+@if ($message = Session::get('success'))
+<div class="alert alert-success alert-block">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <strong>{{ $message }}</strong>
+</div>
+@endif
 
-      @if ($message = Session::get('error'))
-      <div class="alert alert-danger alert-block">
-      <button type="button" class="close" data-dismiss="alert">×</button> 
-      <strong>{{ $message }}</strong>
-      </div>
-    @endif
-      <!-- Main content -->
-      <section class="content">
-        <div class="row">
-          <div class="col-8">
+@if ($message = Session::get('error'))
+<div class="alert alert-danger alert-block">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    <strong>{{ $message }}</strong>
+</div>
+@endif
+<!-- Main content -->
+<section class="content">
+    <div class="row">
+        <div class="col-8">
             <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">List Data Kontak</h3>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-                  <div class="table-responsive">
-                    <table id="datatable" class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Nama</th>
-                                <th>Asal</th>
-                                <th>No Hp</th></th>
-                                <th>Opsi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php
-                                $no = 1;  
-                            @endphp
-                            @foreach ($data as $dt)
+                <div class="card-header">
+                    <h3 class="card-title">List Data Kontak</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="datatable" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Asal</th>
+                                    <th>No Hp</th>
+                                    </th>
+                                    <th>Opsi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                $no = 1;
+                                @endphp
+                                @foreach ($data as $dt)
                                 <tr>
                                     <td>{{$no}}</td>
                                     <td>{{$dt->title}}</td>
@@ -64,63 +65,69 @@
                                         <form action="{{url('admin/kontak/'.$dt->id)}}" method="post">
                                             {{ csrf_field() }}
                                             {{ method_field('DELETE') }}
-                                            <button type="submit" class="btn btn-sm btn-danger" value="delete" title="Delete"><i class="fa fa-trash"></i></button>
+                                            <button type="submit" class="btn btn-sm btn-danger" value="delete"
+                                                title="Delete"><i class="fa fa-trash"></i></button>
                                         </form>
                                     </td>
                                 </tr>
                                 @php
-                                    $no++;
+                                $no++;
                                 @endphp
-                            @endforeach
-                            
-                        </tbody>
-                    </table>
-                  </div>
-              </div>
-              <!-- /.card-body -->
-            </div>
-            <!-- /.card -->
-          </div>
-          <div class="col-4">
-            <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Form Kontak</h3>
-              </div>
-              <form role="form" method="POST" action="{{url('admin/kontak')}}" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                <!-- /.card-header -->
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Kategori</label>
-                        <select class="form-control" name='subparent'>
-                            <option value="callcenter">Call Center</option>
-                            <option value="kontaklain">Kontak Lain</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputEmail1">Atas Nama </label>
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Atas Nama" name="title" required>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputEmail1">Asal (<i>Kosongkan jika memilih kategori callcenter</i>)</label>
-                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Asal Kampus" name="description" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Nomor </label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nomor" name="nomor" required>
+                                @endforeach
+
+                            </tbody>
+                        </table>
                     </div>
                 </div>
                 <!-- /.card-body -->
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-sm btn-primary float-right"><i class="fa fa-save"></i> Simpan</button>
-                </div>
-              </form>
             </div>
             <!-- /.card -->
-          </div>
-          <!-- /.col -->
         </div>
-        <!-- /.row -->
-      </section>
-      <!-- /.content -->
+        <div class="col-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Form Kontak</h3>
+                </div>
+                <form role="form" method="POST" action="{{url('admin/kontak')}}" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Kategori</label>
+                            <select class="form-control" name='subparent'>
+                                <option value="callcenter">Call Center</option>
+                                <option value="kontaklain">Kontak Lain</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Atas Nama </label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Atas Nama"
+                                name="title" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Asal (<i>Kosongkan jika memilih kategori
+                                    callcenter</i>)</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Asal Kampus"
+                                name="description">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Nomor </label>
+                            <input type="number" class="form-control" id="exampleInputEmail1"
+                                placeholder="Contoh: 082180020273" name="nomor" required>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-sm btn-primary float-right"><i class="fa fa-save"></i>
+                            Simpan</button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.card -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+</section>
+<!-- /.content -->
 @endsection
